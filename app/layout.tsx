3acac20be3f6474fbbebfef3e9cfbe00f3d87357
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-09-27 20:02:35 +0700

    update fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,48 +5,60 @@ import 'nextra-theme-docs/style.css'
 import React from 'react'
 
 export const metadata = {
-  title: 'VietnamLLM',
-  description: 'VietnamLLM Documentation'
+    title: 'ViệtLLM',
+    description: 'ViệtLLM Documentation'
 }
 
-const banner = <Banner storageKey="vietnamllm-banner">Welcome to VietnamLLM 🎉</Banner>
+const banner = <Banner storageKey="vietnamllm-banner">Welcome to ViệtLLM 🎉</Banner>
 
 const navbar = (
-  <Navbar
-    logo={<b>VietnamLLM</b>}
+    <Navbar
+        logo={
+            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                <img
+                    src="/favicon/favicon.svg"
+                    alt="ViệtLLM"
+                    style={{ width: '24px', height: '24px' }}
+                />
+                <b>ViệtLLM</b>
+            </div>
+        }
     // ... Your additional navbar options
-  />
+    />
 )
 
-const footer = <Footer>MIT {new Date().getFullYear()} © VietnamLLM.</Footer>
+const footer = <Footer>MIT {new Date().getFullYear()} © ViệtLLM.</Footer>
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html
-      // Not required, but good for SEO
-      lang="en"
-      // Required to be set
-      dir="ltr"
-      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-      suppressHydrationWarning
-    >
-      <Head
-        // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
-      </Head>
-      <body>
-        <Layout
-          banner={banner}
-          navbar={navbar}
-          pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/VietnamLLM/vietnamllm.github.io/tree/main"
-          footer={footer}
-          // ... Your additional layout options
+    return (
+        <html
+            // Not required, but good for SEO
+            lang="en"
+            // Required to be set
+            dir="ltr"
+            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+            suppressHydrationWarning
         >
-          {children}
-        </Layout>
-      </body>
-    </html>
-  )
+            <Head >
+                <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                {/* Your additional tags should be passed as `children` of `<Head>` element */}
+            </Head>
+            <body>
+                <Layout
+                    banner={banner}
+                    navbar={navbar}
+                    pageMap={await getPageMap()}
+                    docsRepositoryBase="https://github.com/VietnamLLM/vietnamllm.github.io/tree/main"
+                    footer={footer}
+                // ... Your additional layout options
+                >
+                    {children}
+                </Layout>
+            </body>
+        </html>
+    )
 }
