5c4c5189d8300dabadf64e8be57c9cdee2a4c90f
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-09-27 19:47:56 +0700

    add update

--- theme.config.jsx
+++ theme.config.jsx
@@ -19,20 +19,23 @@ export default {
   ),
   head: ({ title, meta }) => (
     <>
-      {meta.description && (
-        <meta name="description" content={meta.description} />
-      )}
+      <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      {meta.description && <meta name="description" content={meta.description} />}
       {meta.tag && <meta name="keywords" content={meta.tag} />}
       {meta.author && <meta name="author" content={meta.author} />}
     </>
   ),
-  readMore: 'Read More →',
+  readMore: "Read More →",
   postFooter: null,
   darkMode: false,
   navs: [
     {
-      url: 'https://github.com/VietnamLLM/vietnamllm.github.io',
-      name: 'GitHub'
-    }
-  ]
+      url: "https://github.com/VietnamLLM/vietnamllm.github.io",
+      name: "GitHub",
+    },
+  ],
 }
